bed21a99e5f044c4ff48b1e1212415ff378b38d2
seshukumar291 <seshukumar.kolli@linoy.in>
2019-04-09 22:33:36 +0530

    added inline editing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ChangeDetectionStrategy, OnInit } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit{
 
-  refs = [1,2];
+  refs = [1];
 
   selectedUser;
   users = [];  
   todos = [];
+  selectedTodos = [];
   usersEndPoint = "https://jsonplaceholder.typicode.com/users";
   toDoEndPoint = "https://jsonplaceholder.typicode.com/todos";
   
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef){
       http.get(this.usersEndPoint).subscribe((result: Array<any>) => this.users = [...result]);
-      http.get(this.toDoEndPoint).subscribe((result: Array<any>) => this.todos = [...result]);
+      http.get(this.toDoEndPoint)
+      .pipe(
+        map(
+          (todos: Array<any>) => todos.map(todo => {
+            return {
+              userId: todo.userId,
+              id: todo.id,
+              title: todo.title,
+              isEdit: false
+            } 
+          })
+         )
+      )
+      .subscribe((result: Array<any>) => this.todos = [...result]);
+
+  }
+
+  ngOnInit(){
+    
+  }
+
+  addTodo(userId, title) {
+      this.selectedTodos.push({userId, title, isEdit: false});
   }
 
   selectUser(userId) {
     this.selectedUser = userId;
+  }
+
+  editRow(index) {
+    this.selectedTodos[index].isEdit = true;
+  }
+
+  updateRow(userId, titleId, index){
+    this.selectedTodos[index].userId = userId;
+    this.selectedTodos[index].title = titleId;
   }
 
 }
